c3f6f98db6f6e44e5dcb62e08edb25950c2b7d36
steamebanz_ <evan_pinca@dlsu.edu.ph>
2025-07-25 20:44:44 +0800

    Use multiple xmm registers in grayscale conversion

--- MP2/convert.asm
+++ MP2/convert.asm
@@ -1,29 +1,61 @@
+section .data
+    floatVal dd 255.0          
+
 section .text
 global imgCvtGrayIntToFloat
 
 imgCvtGrayIntToFloat:
-    MOV RSI, RCX ; intPixels
-    MOV RDI, RDX ; floatPixels
-    
-    MOV EAX, R8d ; height
-    IMUL EAX, R9d ; height * width
-    MOV ECX, 0 ; index = 0
-    
-loop_convert:
-    CMP ECX, EAX     
-    JGE end
-    
-    MOV EBX, [RSI + RCX * 4]   
-    CVTSI2SS XMM0, EBX      
-    MOVSS XMM1, [floatVal]     
-    DIVSS XMM0, XMM1             ; xmm0 = pixel / 255.0
-    MOVSS [RDI + RCX * 4], XMM0
+    push rbx
+    push r12
 
-    INC ECX
-    JMP loop_convert
+    mov     rsi, rcx              
+    mov     rdi, rdx              
+    mov     eax, r8d             
+    imul    eax, r9d              
+    xor     ebx, ebx              
 
-end:
-    RET
-    
-section .data 
-    floatVal dd 255.0
+    movss   xmm1, [rel floatVal]  
+
+divide_loop:
+    cmp     ebx, eax
+    jge     done
+
+    ; 1st Pixel
+    mov     r12d, [rsi + rbx*4]
+    cvtsi2ss xmm2, r12d
+    divss   xmm2, xmm1
+    movss   [rdi + rbx*4], xmm2
+    inc     ebx
+    cmp     ebx, eax
+    jge     done
+
+    ; 2nd Pixel
+    mov     r12d, [rsi + rbx*4]
+    cvtsi2ss xmm3, r12d
+    divss   xmm3, xmm1
+    movss   [rdi + rbx*4], xmm3
+    inc     ebx
+    cmp     ebx, eax
+    jge     done
+
+    ; 3rd Pixel
+    mov     r12d, [rsi + rbx*4]
+    cvtsi2ss xmm4, r12d
+    divss   xmm4, xmm1
+    movss   [rdi + rbx*4], xmm4
+    inc     ebx
+    cmp     ebx, eax
+    jge     done
+
+    ; 4th Pixel
+    mov     r12d, [rsi + rbx*4]
+    cvtsi2ss xmm5, r12d
+    divss   xmm5, xmm1
+    movss   [rdi + rbx*4], xmm5
+    inc     ebx
+    jmp     divide_loop
+
+done:
+    pop r12
+    pop rbx
+    ret
